677f0338f4f884d7619aada45abf7412508fe917
Jared Nolen <legodoghead@gmail.com>
2024-07-30 22:13:59 -0700

    Tutorial Function
    - Can go through tutorial
    - Can re-open tutorial through pause menu
    - Will remeber if the player has seen the tutorial and not show it again

--- PirateSoftwareJam/Assets/Scripts/TutorialUI.cs
+++ PirateSoftwareJam/Assets/Scripts/TutorialUI.cs
@@ -4,17 +4,40 @@ using UnityEngine;
 
 public class TutorialUI : MonoBehaviour
 {
-    [SerializeField] private Animator _animator;
+    [SerializeField] private List<GameObject> _tutPages;
+    private int tutPage;
 
     private void Start()
     {
-        GameGrid.Instance.SetInTutorial(true);
+        int seenTutorial = PlayerPrefs.GetInt("TutorialSeen", 0);
+
+        if(seenTutorial == 0)
+            GameGrid.Instance.SetInTutorial(true);
+        else
+            gameObject.SetActive(false);
     }
 
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Space))
-            StartCoroutine(CloseTutorial());
+        {
+            tutPage++;
+
+            if (tutPage >= _tutPages.Count)
+                StartCoroutine(CloseTutorial());
+            else
+                GoToNextPage();
+        }
+    }
+
+    private void GoToNextPage()
+    {
+        foreach (GameObject page in _tutPages)
+        {
+            page.SetActive(false);
+        }
+
+        _tutPages[tutPage].SetActive(true);
     }
 
     private IEnumerator CloseTutorial()
@@ -22,5 +45,20 @@ public class TutorialUI : MonoBehaviour
         yield return null; // Wait a frame so space wont close tut and swap
         gameObject.SetActive(false);
         GameGrid.Instance.SetInTutorial(false);
+        PlayerPrefs.SetInt("TutorialSeen", 1);
+    }
+
+    public void OpenFromStart()
+    {
+        gameObject.SetActive(true);
+        GameGrid.Instance.SetInTutorial(true);
+        tutPage = 0;
+
+        foreach (GameObject page in _tutPages)
+        {
+            page.SetActive(false);
+        }
+
+        _tutPages[tutPage].SetActive(true);
     }
 }
